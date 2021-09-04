cfad647f5e4d4efd06764be3a7ba6b311d3b4059
sohyeonjin <shj41738866@gmail.com>
2021-09-04 17:45:31 +0900

    correct 1-2

--- problemSolving/w1-2.cpp
+++ problemSolving/w1-2.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
-#include <stack>
+#include <vector>
 
 using namespace std;
 
-// 가장 처음에 2번이 들어왔을 경우를 고려해야함.
+/* 
+	스택으로 했을 때는 run-error 가 났었는데 벡터로 바꾸니까 correct됨 이유를 모르겠다
+	가장 처음에 2번이 들어왔을 경우를 고려해야함. (벡터가 비었을 경우) 
+*/
 
 int main() {
 	ios_base::sync_with_stdio(false);
@@ -11,46 +14,53 @@ int main() {
 	cout.tie(NULL);
 
 	int T; // test case 수
-	int c, n;
 	cin >> T;
 
-	for (int t = 0; t < T; t++) {
-		stack<int> stack;
+	for (int t = 0; t < T; t++) {	
 
-		std::stack<int> minstack;
-		std::stack<int> maxstack;
+		vector<int> vector;
 
+		std::vector<int> minvec;
+		std::vector<int> maxvec;
+
+		int c, n;
 		int R; //질의 수
 		cin >> R;
 		int min = 0;
 		int max = 0;
+
 		for (int r = 0; r < R; r++) {
 
 			cin >> c;
 			if (c == 1) {
 				cin >> n; // 1번이면 넣을 숫자 받기
-				stack.push(n);
 
-				if (min==0) { min = n; max = n; minstack.push(n); maxstack.push(n); } //처음 받는경우 초기화
-
+				if (vector.empty()) { //벡터가 비어있을 경우 n으로 min과 max 초기화
+					min = n; max = n; 
+					minvec.push_back(n); maxvec.push_back(n);
+				} 
 				else {
-					if(n < min){min = n; minstack.push(n);}
-					if(n > max){max = n; maxstack.push(n);}
+					if(n <= min){min = n; minvec.push_back(n);}
+					if(n >= max){max = n; maxvec.push_back(n);}
 				}
+
+				vector.push_back(n);
+
 				cout << min << " " << max << "\n";
 			}
 			else {
-				if (min == 0 || stack.empty()) continue; // stack이 비어있을 경우 건너뛰기
+				if (vector.empty()) continue; // 벡터가 비어있을 경우 건너뛰기
 
-				int top = stack.top();
-				stack.pop();
-				if (top == min) {
-					minstack.pop();
-					min = minstack.top();
+				int top = vector.back();
+				vector.pop_back();
+
+				if (top == min) { // 제거한 값이 최솟값이었을 경우 min값을 이전 최솟값으로 되돌림
+					minvec.pop_back();
+					min = minvec.back(); 
 				}
 				if (top == max) {
-					maxstack.pop();
-					max = maxstack.top();
+					maxvec.pop_back();
+					max = maxvec.back();
 				}
 			}
 		}
