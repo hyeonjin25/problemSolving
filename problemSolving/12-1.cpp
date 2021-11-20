f6400720aca3a685fa6867e7d832f8aafcca21e0
sohyeonjin <shj41738866@gmail.com>
2021-11-20 22:50:00 +0900

    correct

--- problemSolving/12-1.cpp
+++ problemSolving/12-1.cpp
@@ -4,16 +4,7 @@ using namespace std;
 
 int n; //삼각형의 행의 수
 int arr[101][101]; //삼각형을 이루는 이차원 배열 0번쨰는 사용안함
-int min_sum = 10000000;
 
-void recur(int cur_i, int cur_j, int sum) {
-    if (cur_i == n) { //마지막 행이라면
-        min_sum = min(min_sum, sum + arr[cur_i][cur_j]); // 경로의 합이 가장 크다면 min_sum 갱신
-        return;
-    }
-    recur(cur_i + 1, cur_j, sum + arr[cur_i][cur_j]);
-    recur(cur_i + 1, cur_j + 1, sum + arr[cur_i][cur_j]);
-}
 
 int main() {
     ios_base::sync_with_stdio(false);
@@ -32,9 +23,29 @@ int main() {
                 cin >> arr[i][j];
             }
         }
+        
+        for (int i = 2; i < n + 1; i++) { // 두번쨰 줄 부터 시작
+            for (int j = 1; j < i + 1; j++) {
+                if (j == 1) { // 가장 왼쪽일 경우
+                    arr[i][j] = arr[i - 1][j] + arr[i][j]; // 그냥 바로 위의 값과 더해서 갱신
+                }
+                else if (j == i) {  // 가장 오른쪽일 경우
+                    arr[i][j] = arr[i - 1][j - 1] + arr[i][j]; // 그냥 바로 위의 왼쪽 값과 더해서 갱신
+                }
+                else {
+                    // 현재 위치의 값을 위에서 더해서 내려올 수 있는 최소 값으로 바꾸기 
+                    // (내려올 수 있는 위의 두 값 중 더 작은 값과 자신을 더한 값으로 갱신)
+                    arr[i][j] = min(arr[i - 1][j - 1], arr[i - 1][j]) + arr[i][j];
+                }
+            }
+        }
 
-        min_sum = 10000000;
-        recur(1, 1, 0);
-        cout << min_sum << "\n";
+        // 맨 마지막 행에서 가장 작은 값 찾기
+        int min = 10000000;
+        for (int i = 1; i < n + 1; i++) {
+            if (min > arr[n][i]) min = arr[n][i];
+        }
+
+        cout << min << "\n";
     }
 }
