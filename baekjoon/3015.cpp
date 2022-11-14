ce49dc70262f50495be11a19405cfa5cd90fca4b
hyeonjin25 <shj41738866@gmail.com>
2022-11-14 22:08:48 +0900

    3015

---
+++ baekjoon/3015.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  int n;
+  long long t, res = 0;
+  stack<pair<int, int>> S; // 키, 같은키의 수
+
+  cin >> n;
+
+  // 543321 3
+
+  while (n--)
+  {
+    cin >> t;
+
+    while (S.size() && S.top().first < t)
+    {
+      res += S.top().second;
+      S.pop();
+    }
+
+    if (S.size() && S.top().first == t)
+    {
+      res += S.top().second;
+      if (S.size() > 1)
+        res++;
+      S.top().second++;
+    }
+    else
+    {
+      if (S.size())
+        res++;
+      S.push({t, 1});
+    }
+  }
+
+  cout << res;
+}
