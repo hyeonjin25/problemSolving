3b95e638e297f482914b134f47ee838b52598fce
hyeonjin25 <shj41738866@gmail.com>
2022-12-01 19:27:41 +0900

    1021

---
+++ baekjoon/1021.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  int n, m, t, res = 0;
+  deque<int> D;
+
+  cin >> n >> m;
+
+  for (int i = 1; i < n + 1; i++)
+  {
+    D.push_back(i);
+  }
+
+  // 2489
+
+  while (m--)
+  {
+    cin >> t;
+
+    while (D.front() != t)
+    {
+      // 찾는 원소가 front쪽에 더 가까이 있는 경우
+      if (t - D.front() <= D.back() - t)
+      {
+        D.push_back(D.front());
+        D.pop_front();
+      }
+      else
+      {
+        D.push_front(D.back());
+        D.pop_back();
+      }
+      res++;
+    }
+    D.pop_front();
+  }
+
+  cout << res;
+}
