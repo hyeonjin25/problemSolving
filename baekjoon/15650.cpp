aa033b37adc05d06f61696d8568c4184b0856995
hyeonjin25 <shj41738866@gmail.com>
2023-02-03 16:39:05 +0900

    15650

---
+++ baekjoon/15650.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n,m;
+int isused[10];
+int arr[10];
+
+void func(int k){
+  if(k==m){
+    for(int i=0;i<m;i++)
+      cout << arr[i] << ' ';
+     cout << '\n';
+    return;
+  }
+
+  for(int i=arr[k-1]+1;i<n+1;i++){
+      if(!isused[i]){
+        isused[i]=1;
+        arr[k]=i;
+        func(k+1);
+        isused[i]=0;
+      }
+    }
+}
+
+
+int main(){
+  ios::sync_with_stdio(); cin.tie(0);
+
+  cin >> n>>m;
+  func(0);
+}
