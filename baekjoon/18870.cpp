9c8ffb422bee5fad00919f81467c1dd30d263a8a
hyeonjin25 <shj41738866@gmail.com>
2023-02-25 00:13:08 +0900

    18870

---
+++ baekjoon/18870.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int add[1000005];
+int main(){
+  ios::sync_with_stdio(0); cin.tie(0);
+
+  int n; cin >> n;
+  for(int i=0; i<n;i++){
+    cin >> arr[i];
+  }
+
+  sort(arr,arr+n);
+
+  
+}
