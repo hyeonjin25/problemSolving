6189c74b08ed1180dfd49160b8b708f052cf8441
hyeonjin25 <shj41738866@gmail.com>
2023-03-02 19:25:46 +0900

    2295

---
+++ baekjoon/2295.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int b_search(int a){
+  int st=0,en=n-1;
+  while(st<=en){
+    int mid = (st+en)/2;
+    if(mid>target) en=mid-1;
+    else if(mid<target) en=
+  }
+
+int main(){
+  ios::sync_with_stdio(0); cin.tie(0);
+
+  for(int i=0; i<)
+}
